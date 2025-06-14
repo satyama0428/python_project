d43c08cbf7a29dcf978a7adc6d18608a33bbfb75
satyam <satyam0496@gmail.com>
2025-06-14 05:54:07 +0530

    Initial Python project structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_project',
+    version='0.1.0',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests"
+    ],
+    entry_points={
+        'console_scripts': [
+            'mycli = main:main'
+        ]
+    }
+)
+
